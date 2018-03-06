fe74725383496ea559c424641e227ce835486abd
luciancrasovan <lucian.crasovan@admanmedia.com>
2018-03-06 01:12:30 +0100

    more tests with palindromesgit add .!

--- test/serpent_talk/core_test.clj
+++ test/serpent_talk/core_test.clj
@@ -7,5 +7,21 @@
     (is (= nil ((fn foo [x] (when (> x 0) (conj (foo (dec x)) x))) 0)))
     (is (= '(2 1) ((fn foo [x] (when (> x 0) (conj (foo (dec x)) x))) 2)))
     (is (= '(5 4 3 2 1) ((fn foo [x] (when (> x 0) (conj (foo (dec x)) x))) 5)))
-    ))
+    )
 
+  (testing "thread-like stuff"
+    (is (= (reduce + 0 (map inc (take 3 (drop 2 [2 5 4 1 3 6]))))
+           (->> [2 5 4 1 3 6] (drop 2) (take 3) (map inc) (reduce + 0))
+           11))
+    )
+
+  (testing "for the win"
+    (is (= [1 5 9 13 17 21 25 29 33 37] (for [x (range 40)
+                    :when (= 1 (rem x 4))]
+                x))))
+
+  (testing "palindrome"
+    (is (= true (#(= (clojure.string/reverse %) %) "paap"))  )
+  )
+
+  )
