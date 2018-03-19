cca9db60781b0caf2f32ec166d129a6b158ba0ab
luciancrasovan <lucian.crasovan@admanmedia.com>
2018-03-20 00:39:06 +0100

    Iteration introduced

--- test/serpent_talk/core_test.clj
+++ test/serpent_talk/core_test.clj
@@ -87,4 +87,7 @@
               ) 7) [1 1 2 3 5 8 13]))
     )
 
+  (testing "writes a functions using iterate that returns the first n fibonacci numbers"
+    (is (= (take 7 (map first (iterate (fn [[a b]] [b (+' a b)]) [0 1]))) [0 1 1 2 3 5 8]))
+    )
   )
